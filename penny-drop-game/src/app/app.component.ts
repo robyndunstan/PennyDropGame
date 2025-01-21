7b590b74b43f40b9eb3d09c7d0937131a9cf4821
Dunstan <DunstanR@members1st.org>
2025-01-21 15:09:08 -0500

    use font awesome icon

--- penny-drop-game/src/app/app.component.ts
+++ penny-drop-game/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDiceTwo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'penny-drop-game';
+  faDiceTwo = faDiceTwo;
 }
